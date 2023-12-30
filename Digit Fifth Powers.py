b529483a54bdc643ee397af034f5b969fbeb52a3
Mehmet Karakoca <sykrkc@gmail.com>
2023-12-30 16:46:52 +0300

    Digit Fifth Powers

---
+++ Digit Fifth Powers.py
@@ -0,0 +1,13 @@
+n = 2
+control_n = 0
+sum = 0
+while n < 1000000:
+    for i in str(n):
+        control_n += (int(i) ** 5)
+    else:
+        if control_n == n:
+            print("sayı budur",n)
+            sum += n
+    control_n = 0       
+    n = n + 1               
+print(sum)
